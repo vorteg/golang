7ab72886efdeee71433b39a162d0e45a6c888983
Victor Ortega <victor.ortega@delphiansystems.com>
2021-12-23 14:46:51 -0600

    adding a break from loop

--- hello-world-GO/main.go
+++ hello-world-GO/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"myapp/doctor"
 	"os"
+	"strings"
 )
 
 // Simple version of a loop
@@ -16,9 +17,20 @@ func main() {
 	fmt.Println(whatToSay)
 
 	for {
+		fmt.Print("->")
 		userInput, _ := reader.ReadString('\n')
+		println(userInput)
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		println(userInput)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		println(userInput)
 
-		fmt.Println("User commented: " + userInput)
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+
 	}
 
 }
