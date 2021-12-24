6fc4b42d552c1c877a866576b0385f0cfcc7bdd0
Victor Ortega <victor.ortega@delphiansystems.com>
2021-12-24 14:35:13 -0600

    Full Guess Number Program, using const and operatores

--- variables/main.go
+++ variables/main.go
@@ -1,14 +1,44 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//const never change
+const prompt = "and press ENTER when ready."
+
 func main() {
-	// one way -declare, then assign (steps)
-	var firstNumber int
-	firstNumber = 2
-
-	// another way, declare type and name and assign value
+	var firstNumber = 2
 	var secondNumber = 5
+	var subtraction = 7
+	var answer int
 
-	// one step variable: declare name, assign value and let Go figure out the type
-	subtraction := 7
+	reader := bufio.NewReader((os.Stdin))
 
+	// display a welcome/instructions
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number betwen 1 to 10", prompt)
+	reader.ReadString('\n')
+
+	// take them through the games
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	// give them the answer
+	answer = firstNumber*secondNumber - subtraction
+	fmt.Println("The answer is", answer)
 }
