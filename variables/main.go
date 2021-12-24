dfe90e7f590d43ffc5870cb2a95371de2e44b519
Victor Ortega <victor.ortega@delphiansystems.com>
2021-12-24 12:05:58 -0600

    Session 2 - Different ways to define a variables

---
+++ variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// one way -declare, then assign (steps)
+	var firstNumber int
+	firstNumber = 2
+
+	// another way, declare type and name and assign value
+	var secondNumber = 5
+
+	// one step variable: declare name, assign value and let Go figure out the type
+	subtraction := 7
+
+}
