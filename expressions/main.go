1a2cd3f377366b1c2671e6f0b4cc279d9fa33d41
Victor Ortega <victor.ortega@delphiansystems.com>
2022-01-01 18:45:37 -0600

    Compund booleans: mixing boland operations

--- expressions/main.go
+++ expressions/main.go
@@ -2,27 +2,47 @@ package main
 
 import "fmt"
 
+type Employee struct {
+	Name     string
+	Age      int
+	Salary   int
+	FullTime bool
+}
+
 func main() {
-	apples := 18
-	oranges := 9
+	jack := Employee{
+		Name:     "Jack Smith",
+		Age:      27,
+		Salary:   40000,
+		FullTime: false,
+	}
 
-	// boolean expression
-	fmt.Println(apples == oranges)
-	fmt.Println(apples != oranges)
+	jill := Employee{
+		Name:     "Jill Jones",
+		Age:      33,
+		Salary:   60000,
+		FullTime: true,
+	}
 
-	//This operation dosen't allow
-	// if apples == "10"{
+	var employees []Employee
+	employees = append(employees, jack)
+	employees = append(employees, jill)
 
-	// }
+	for _, x := range employees {
+		if x.Age > 30 {
+			fmt.Println(x.Name, "is 30 or older")
+		} else {
+			fmt.Println(x.Name, "is under 30")
+		}
 
-	// >< >= <=
-	fmt.Printf("%d > %d: %t", apples, oranges, apples > oranges)
-	fmt.Println()
-	fmt.Printf("%d < %d: %t", apples, oranges, apples < oranges)
-	fmt.Println()
-	fmt.Printf("%d >= %d: %t", apples, oranges, apples >= oranges)
-	fmt.Println()
-	fmt.Printf("%d <= %d: %t", apples, oranges, apples <= oranges)
-	fmt.Println()
+		if x.Age > 30 && x.Salary > 50000 {
+			fmt.Println(x.Name, "makes more than 50000 or is over 30")
+		} else {
+			fmt.Println("Either", x.Name, "Makes less than 50000 and is under 30 ")
+		}
 
+		if (x.Age > 30 || x.Salary < 50000) && x.FullTime {
+			fmt.Println(x.Name, "match our unclear criteria")
+		}
+	}
 }
