395e058f10959653bc63e6156590e5cdfc1f79c3
Victor Ortega <victor.ortega@delphiansystems.com>
2022-01-01 18:22:06 -0600

    what is an expression

---
+++ expressions/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 10
+	name := "jack"
+	rightHanded := true
+	//This is an expression ->
+	fmt.Printf("%s is %d years old. Right handed: %t", name, age, rightHanded)
+	//This another expresion
+	ageTeen := age >= 20
+
+}
