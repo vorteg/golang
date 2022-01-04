9739a33639704e8ac36e1b1b5385c0e99f1d109d
Victor Ortega <victor.ortega@delphiansystems.com>
2022-01-04 12:29:49 -0600

    Three basic loops

---
+++ three-part-loop/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//infinit loop
+	// for {
+
+	// }
+	// WHen we have an index
+	// for _, x := range myMap {
+
+	// }
+
+	//having a condition
+	for i := 0; i <= 10; i++ {
+		fmt.Println("i is ", i)
+	} // also is posible use i-- or i= i-1
+
+}
