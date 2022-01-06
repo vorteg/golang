9eddba4035dce4779236cc7bdb4662ec92387675
Victor Ortega <victor.ortega@delphiansystems.com>
2022-01-05 18:22:45 -0600

    switch case

--- rock-papar-scissors/main.go
+++ rock-papar-scissors/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -25,41 +26,82 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	//clearScreen()
+	clearScreen()
 
 	fmt.Print("Please enter rock, paper, or scissors ->")
 	playerChoice, _ = reader.ReadString('\n')
+	playerChoice = strings.Replace(playerChoice, "\r\n", "", -1)
 	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
 
-	//clearScreen()
 	if playerChoice == "rock" {
 		playerValue = ROCK
+		fmt.Println("Player chose", playerValue)
 	} else if playerChoice == "paper" {
 		playerValue = PAPER
+		fmt.Println("Player chose", playerValue)
 	} else if playerChoice == "scissors" {
 		playerValue = SCISSORS
+		fmt.Println("Player chose", playerValue)
 	}
 
-	fmt.Println()
-	fmt.Println("Player chose", playerChoice, "and value is", playerValue)
-	fmt.Println("Computer chose", computerValue)
+	switch computerValue {
+	case ROCK:
+		fmt.Println("Computer chose Rock")
+		break
+	case PAPER:
+		fmt.Println("Computer chose Paper")
+		break
+	case SCISSORS:
+		fmt.Println("Computer chose Scissors")
+		break
+	default:
+	}
+
+	if playerValue == computerValue {
+		fmt.Println("It's a draw")
+	} else {
+		switch playerValue {
+		case ROCK:
+			if computerValue == PAPER {
+				fmt.Println("Computer wins!")
+			} else {
+				fmt.Println("Player wins!")
+			}
+			break
+		case PAPER:
+			if computerValue == SCISSORS {
+				fmt.Println("Computer wins!")
+			} else {
+				fmt.Println("Player wins!")
+			}
+
+			break
+		case SCISSORS:
+			if computerValue == ROCK {
+				fmt.Println("Computer wins!")
+			} else {
+				fmt.Println("Player wins!")
+			}
+
+			break
+		default:
+			fmt.Println("Invalid choise")
+		}
+	}
 
 }
 
 // clearScreen clears the screen
 func clearScreen() {
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-	// if strings.Contains(runtime.GOOS, "windows") {
-	// 	// windows
-	// 	cmd := exec.Command("clear")
-	// 	cmd.Stdout = os.Stdout
-	// 	cmd.Run()
-	// } else {
-	// 	// linux or mac
-	// 	cmd := exec.Command("clear")
-	// 	cmd.Stdout = os.Stdout
-	// 	cmd.Run()
-	// }
+	if strings.Contains(runtime.GOOS, "windows") {
+		// windows
+		cmd := exec.Command("cmd", "c/", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		// linux or mac
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
