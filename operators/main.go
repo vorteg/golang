63f24cc69ab8dd08b97bbb237ab2533e86c4d36a
Victor Ortega <victor.ortega@delphiansystems.com>
2022-01-10 10:19:13 -0600

    Commun operations

--- operators/main.go
+++ operators/main.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	answer := 7 + 3*4
-	fmt.Println("Answer:", answer)
+	// multiplication
+	// area = nr2
+	var radius = 12.0
+	area := math.Pi * radius * radius
+	fmt.Println("Area is ", area)
 
-	answer = (7 + 3) * 4
-	fmt.Println("Answer is now:", answer)
+	// integer division
+	half := 1 / 2
+	fmt.Println("half with integer division", half)
+
+	// float division
+	halfFloat := 1.0 / 2.0
+	fmt.Println("half float", halfFloat)
+
+	//squering (raising to the power)
+	badThreeSquared := 3 ^ 2 // this in reality a bit operator
+	fmt.Println("badThreeSquared ", badThreeSquared)
+	goodThreeSquered := math.Pow(3.0, 2.0)
+	fmt.Println("goodThreeSquared", goodThreeSquered)
+
+	// modulus
+	remainder := 50 % 3
+	fmt.Println("remainder", remainder)
+
+	// unary operators
+	x := 3
+	x++
+	fmt.Println("x is now", x)
+
+	x--
+	x--
+	fmt.Println("x is now ", x)
 }
