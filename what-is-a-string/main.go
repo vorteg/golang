f08c495111d60abe49d0e9d8a31b6bc3ee47482e
Victor Ortega <victor.ortega@delphiansystems.com>
2022-01-11 12:59:13 -0600

    String index

--- what-is-a-string/main.go
+++ what-is-a-string/main.go
@@ -1,56 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	fmt.Println()
-	name := "Hello world"
-	fmt.Println("String:", name)
-	fmt.Println()
+	// index start at 0
+	//                       1         2         3
+	//             01234567890123456789012345678901234
 
-	fmt.Println("Bytes")
-	for i := 0; i < len(name); i++ {
-		fmt.Printf("%x \n", name[i])
+	courseName := "Learn Go for Beginners Crash Course"
+	fmt.Println(string(courseName[0]))
+
+	for i := 0; i <= 21; i++ {
+		fmt.Print(string(courseName[i]))
 	}
-
-	tab_char(name)
-	name = "Γειά σου Κόσμε"
-	tab_char(name)
-
-	// Three ways to concatenate strings
-	fmt.Println("Three ways to concatenate strings")
-	h := "Hello, "
-	w := "world."
-
-	//using + not very efficient
-	myString := h + w
-	fmt.Println(myString)
-	fmt.Println()
-
-	//using fmt - more efficient
-	myString = fmt.Sprintf("%s%s", h, w)
-	fmt.Println(myString)
-	fmt.Println()
-
-	//using stringBuilder - very efficient
-	var sb strings.Builder
-	sb.WriteString(h)
-	sb.WriteString(w)
-	fmt.Println(sb.String())
-
-	fmt.Println()
-	name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	fmt.Println("Getting a substring")
-	fmt.Println(name[0:13])
-}
-
-func tab_char(name string) {
-	fmt.Println("Index\tRune\tString")
-	for x, y := range name {
-		fmt.Println(x, "\t", y, "\t", string(y))
-	}
-	fmt.Println()
 }
